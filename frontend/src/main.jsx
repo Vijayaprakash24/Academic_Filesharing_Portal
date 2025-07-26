7fe6a122529ef5dded32f928dff65e6a3835d46f
vijayapraksh24 <vijayaprakash.m.v.s@gmail.com>
2025-07-26 22:15:00 +0530

    first

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-   
+createRoot(document.getElementById("root")).render(
+   <StrictMode>
     <App />
+  </StrictMode>
 
 );
