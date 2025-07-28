c9d94297272625334296517d2c5ad1df0f210a89
vijayapraksh24 <vijayaprakash.m.v.s@gmail.com>
2025-07-28 17:42:34 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src') 
+      '@': path.resolve(__dirname, 'src'),
     }
-  },
-  server: {
-   
-    historyApiFallback: true
   }
-})
+});
