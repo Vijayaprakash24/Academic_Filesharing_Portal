755c15ceef68836617d4bf9496478ec1b74ed991
vijayapraksh24 <vijayaprakash.m.v.s@gmail.com>
2025-07-28 18:03:52 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     }
   },
-  publicDir: 'public',
-});
+  build: {
+    outDir: 'dist',
+  },
+  base: './', // important for relative asset paths in production!
+})
